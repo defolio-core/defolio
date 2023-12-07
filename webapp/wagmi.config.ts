59701d071d9b135db1f54af16422ed1b00d738d2
strandgeek <strandgeek@gmail.com>
2023-12-07 06:04:20 -0500

    feat(webapp): add factory contract to wagmi-cli
    Changes:
    
    * Add the factory contract to wagmi-cli config

--- webapp/wagmi.config.ts
+++ webapp/wagmi.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from '@wagmi/cli';
 import { react } from '@wagmi/cli/plugins';
 import DeFolioSpace from './src/abi/DeFolioSpace.json';
+import DeFolioSpaceFactory from './src/abi/DeFolioSpaceFactory.json';
  
 export default defineConfig({
   out: 'src/generated.ts',
@@ -8,6 +9,10 @@ export default defineConfig({
     {
       name: 'DeFolioSpace',
       abi: DeFolioSpace,
+    },
+    {
+      name: 'DeFolioSpaceFactory',
+      abi: DeFolioSpaceFactory,
     }
   ],
   plugins: [
